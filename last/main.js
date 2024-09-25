063d9a9861bba842ff8a556b60980fedaadcce58
ZeroMAN555 <164915765+ZeroMAN555@users.noreply.github.com>
2024-09-25 18:08:41 +0700

    Add files via upload

---
+++ last/main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Happy Aniversary Bebeeyyyyyy", "Aku Sayang Banget Sama Kamu", "Aku Minta Maaf Ya", "Kalau Aku Udah Buat Kamu Sedih" ,"Kau tau..." ,"Kamu Sudah Jadi Bagian Di Hidupku", "Terimakasih Untuk Semuanya Ya", "love u so much:)"],
+        typeSpeed: 50
+    });
+}
